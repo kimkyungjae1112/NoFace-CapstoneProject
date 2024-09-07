e752cdee206f48f135b3c6c8304f555a10a3aee6
kimkyungjae1112 <74956602+kimkyungjae1112@users.noreply.github.com>
2024-09-08 00:13:50 +0900

    feat : 스킬 헤더파일 모음 헤더 작성

--- Source/CapstoneProject/Character/CharacterBase.cpp
+++ Source/CapstoneProject/Character/CharacterBase.cpp
@@ -10,11 +10,7 @@
 #include "InputMappingContext.h"
 #include "Player/CPlayerController.h"
 #include "Blueprint/AIBlueprintHelperLibrary.h"
-#include "Skill/Skill_Q_ChargeAttack.h"
-#include "Skill/Skill_Q_DoubleAttack.h"
-#include "Skill/Skill_W_UnConfirmed.h"
-#include "Skill/Skill_E_UnConfirmed.h"
-#include "Skill/Skill_R_UnConfirmed.h"
+#include "SkillHeader/CSkillHeader.h"
 #include "Stat/CharacterStatComponent.h"
 
 ACharacterBase::ACharacterBase()
