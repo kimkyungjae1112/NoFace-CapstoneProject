70e754194f52a1b2caa914826f70c21b444bf992
kimkyungjae1112 <74956602+kimkyungjae1112@users.noreply.github.com>
2024-09-09 12:39:22 +0900

    feat : AI Detect 기능 추가

--- Source/CapstoneProject/AI/Common/BTService_Detect.cpp
+++ Source/CapstoneProject/AI/Common/BTService_Detect.cpp
@@ -2,4 +2,54 @@
 
 
 #include "AI/Common/BTService_Detect.h"
+#include "AIController.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "Interface/AIInterface.h"
+#include "Engine/OverlapResult.h"
 
+UBTService_Detect::UBTService_Detect()
+{
+	NodeName = TEXT("Detect");
+	Interval = 1.f;
+}
+
+void UBTService_Detect::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+{
+	Super::TickNode(OwnerComp, NodeMemory, DeltaSeconds);
+
+	APawn* PossessedPawn = OwnerComp.GetAIOwner()->GetPawn();
+	if (PossessedPawn == nullptr)
+	{
+		return;
+	}
+
+	UWorld* World = PossessedPawn->GetWorld();
+	if (World == nullptr)
+	{
+		return;
+	}
+
+	IAIInterface* Interface = Cast<IAIInterface>(PossessedPawn);
+	if (Interface == nullptr)
+	{
+		return;
+	}
+
+	float Radius = Interface->GetDetectRadius();
+	FVector Origin = PossessedPawn->GetActorLocation();
+	TArray<FOverlapResult> OverlapResults;
+	FCollisionQueryParams Params(NAME_None, false, PossessedPawn);
+	
+	bool bOverlap = World->OverlapMultiByChannel(OverlapResults, Origin, FQuat::Identity, ECC_GameTraceChannel1, FCollisionShape::MakeSphere(Radius), Params);
+	if (bOverlap)
+	{
+		for (auto const& OverlapResult : OverlapResults)
+		{
+			APawn* Target = Cast<APawn>(OverlapResult.GetActor());
+			OwnerComp.GetBlackboardComponent()->SetValueAsObject(TEXT("Target"), Target);
+
+			return;
+		}
+	}
+	OwnerComp.GetBlackboardComponent()->SetValueAsObject(TEXT("Target"), nullptr);
+}
--- Source/CapstoneProject/AI/Common/BTService_Detect.h
+++ Source/CapstoneProject/AI/Common/BTService_Detect.h
@@ -13,5 +13,10 @@ UCLASS()
 class CAPSTONEPROJECT_API UBTService_Detect : public UBTService
 {
 	GENERATED_BODY()
+
+public:
+	UBTService_Detect();
+
+	virtual void TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) override;
 	
 };
