0bd758a4fb1f465876e237225c2074a29a2f559d
kimkyungjae1112 <rudwo4861@naver.com>
2024-09-28 21:07:29 +0900

    feat : 탱커 적 클래스, 컨트롤러 추가

---
+++ Source/CapstoneProject/AI/Controller/AIControllerTanker.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AI/Controller/AIControllerBase.h"
+#include "AIControllerTanker.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CAPSTONEPROJECT_API AAIControllerTanker : public AAIControllerBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/CapstoneProject/Enemy/EnemyMelee_Tanker.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Enemy/EnemyBase.h"
+#include "EnemyMelee_Tanker.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CAPSTONEPROJECT_API AEnemyMelee_Tanker : public AEnemyBase
+{
+	GENERATED_BODY()
+	
+};
